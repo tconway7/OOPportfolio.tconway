08a8d4660c5f2aff7570b58df16e650521a0e585
tconway7 <146092525+tconway7@users.noreply.github.com>
2024-02-01 01:29:41 +0000

    Personal details and test

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails/src/Personaldetails.java
@@ -0,0 +1,13 @@
+
+public class Personaldetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Name: John Smith");
+		System.out.println ("Address: 34 New Street");
+		System.out.println ("Age: 56");
+		System.out.println ("Phone number: 076335767877");
+		System.out.println ("Postcode: DE4 HG5");
+	}
+
+}
---
+++ WeeklyExercises/1GettingStarted/Test/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test
+{
+   //----------------------------------------------------
+   //  Prints a statement.
+   //----------------------------------------------------
+   public static void main (String[] args)
+   {
+      System.out.println ("An Emergency Broadcast");
+   }
+}
