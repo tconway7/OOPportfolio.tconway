5d66f60b5226cf1acf94d4ebfce1d98b7865436d
tconway7 <tom.conway05@outlook.com>
2024-04-09 17:23:56 +0100

    Lab 5 Bulb 9/04

--- WeeklyExercises/5_6WritingClassesAndMethods/Bulb/src/Bulb.java
+++ WeeklyExercises/5_6WritingClassesAndMethods/Bulb/src/Bulb.java
@@ -1,2 +1,28 @@
-public class Bulb {0
+public class Bulb {
+    private boolean switchIsOn;
+    private String BulbName;
+
+    public Bulb(String BulbName) {
+        this.BulbName = BulbName;
+        this.switchIsOn = false;
+    }
+
+    public void turnOn() {
+        this.switchIsOn = true;
+        System.out.println(BulbName + " light is switched on.");
+    }
+
+    public void turnOff() {
+        this.switchIsOn = false;
+        System.out.println(BulbName + " light is switched off.");
+    }
+    public String switchIsOn() {
+        if (this.switchIsOn) {
+            return (BulbName + " light is on.");
+        } else {
+            return (BulbName + " light is off.");
+        }
+    }
+
+
 }
--- WeeklyExercises/5_6WritingClassesAndMethods/Bulb/src/Lights.java
+++ WeeklyExercises/5_6WritingClassesAndMethods/Bulb/src/Lights.java
@@ -1,2 +1,15 @@
+
 public class Lights {
+    public static void main(String[] args) {
+        Bulb bulb1 = new Bulb("Bedroom");
+        Bulb bulb2 = new Bulb("Bathroom");
+        Bulb bulb3 = new Bulb("Kitchen");
+
+        bulb1.turnOn();
+        bulb2.turnOn();
+
+        System.out.println(bulb1.switchIsOn());
+
+    }
 }
+
